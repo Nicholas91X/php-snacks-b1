29027292e081d4a7b94fa1c10def0d8d4533fdcd
Nicholas <romitinicholas@gmail.com>
2022-01-18 17:21:36 +0100

    secondo snack versione senza età

--- snack 2/index.php
+++ snack 2/index.php
@@ -1,2 +1,10 @@
 <!-- Snack 2
 Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+<?php
+if (strlen($_GET['nome']) > 3 && stripos($_GET['mail'], '@') !== false) {
+    echo "Accesso Consentito";
+} else {
+    echo "Accesso Negato";
+}
+?>
